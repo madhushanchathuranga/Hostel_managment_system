b1dc3254c7a84e598713c00aa1d4a6b77fcc57a9
A.V.G.M.Chathuranga <79782422+madhushanchathuranga@users.noreply.github.com>
2023-11-11 10:49:26 +0530

    simple project

---
+++ hostel/src/test/java/com/ictec/hostel/HostelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ictec.hostel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HostelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
