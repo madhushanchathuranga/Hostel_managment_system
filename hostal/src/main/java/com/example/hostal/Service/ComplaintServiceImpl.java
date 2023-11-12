e9f890e3367be77ad16e2509b588b6111dd0cc4e
A.V.G.M.Chathuranga <79782422+madhushanchathuranga@users.noreply.github.com>
2023-11-12 09:58:39 +0530

    Update ComplaintServiceImpl.java

--- hostal/src/main/java/com/example/hostal/Service/ComplaintServiceImpl.java
+++ hostal/src/main/java/com/example/hostal/Service/ComplaintServiceImpl.java
@@ -1,4 +1,26 @@
 package com.example.hostal.Service;
 
 public class ComplaintServiceImpl {
+    @Autowired
+    private ComplaintRepository complaintRepository;
+
+    public ComplaintServiceImpl(ComplaintRepository complaintRepository){
+        super();
+        this.complaintRepository=complaintRepository;
+    }
+    @Override
+    public  Complaint saveComplaint(Complaint complaint) {
+        return complaintRepository.save(complaint);
+    }
+    @Override
+    public Iterable<Complaint> getAllComplaint() {
+        return (List<Complaint>) complaintRepository.findAll();
+    }
+
+
+    @Override
+    public Complaint getComplaintById(Long id) {
+        Optional<Complaint> complaintOptional = complaintRepository.findById(id);
+        return complaintOptional.orElse(null);
+    }
 }
