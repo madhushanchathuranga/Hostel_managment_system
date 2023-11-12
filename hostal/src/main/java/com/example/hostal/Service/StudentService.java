3dbe8339b9bc89f5def76b887ecc352731fac032
A.V.G.M.Chathuranga <79782422+madhushanchathuranga@users.noreply.github.com>
2023-11-12 10:11:01 +0530

    Update StudentService.java

--- hostal/src/main/java/com/example/hostal/Service/StudentService.java
+++ hostal/src/main/java/com/example/hostal/Service/StudentService.java
@@ -1,4 +1,11 @@
 package com.example.hostal.Service;
+import com.example.hostal.Entity.Student;
 
 public interface StudentService {
+    Student saveStudent(Student student);
+
+    Iterable<Student> getAllStudent();
+
+    Student getStudentById(Integer id);
 }
+
