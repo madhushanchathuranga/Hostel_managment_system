a0b2789c7f277f8869e328f4b6848394f49c14a8
A.V.G.M.Chathuranga <79782422+madhushanchathuranga@users.noreply.github.com>
2023-11-12 10:12:48 +0530

    Update UserService.java

---
+++ hostal/src/main/java/com/example/hostal/Service/UserService.java
@@ -0,0 +1,7 @@
+//package com.example.hostal.Service;
+//
+//import com.example.hostal.Model.User;
+//
+//public interface UserService extends UserDetailsService{
+//    User save(UserRegistrationDto registrationDto);
+//}
