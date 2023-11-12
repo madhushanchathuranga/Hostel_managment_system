1eb7f5b9de940ebe7337118dfcfb198172ef9ae5
A.V.G.M.Chathuranga <79782422+madhushanchathuranga@users.noreply.github.com>
2023-11-12 14:26:35 +0530

    Create HostalApplicationTests.java

---
+++ hostal/src/test/java/com/example/hostal/HostalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hostal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HostalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
