7ccc5f8a68a4fff936f10bbf207e0f6d2f23eba4
A.V.G.M.Chathuranga <79782422+madhushanchathuranga@users.noreply.github.com>
2023-11-12 10:08:03 +0530

    Update ReportService.java

--- hostal/src/main/java/com/example/hostal/Service/ReportService.java
+++ hostal/src/main/java/com/example/hostal/Service/ReportService.java
@@ -1,4 +1,11 @@
 package com.example.hostal.Service;
 
+
+import com.example.hostal.Entity.Report;
 public interface ReportService {
+    Report saveReport(Report report);
+
+    Iterable<Report> getAllReport();
+
+    Report getReportById(Long id);
 }
