1cac3693fac899263f4607a8c8b4cc54011f2919
A.V.G.M.Chathuranga <79782422+madhushanchathuranga@users.noreply.github.com>
2023-11-12 10:07:27 +0530

    Update PropertyServiceImpl.java

--- hostal/src/main/java/com/example/hostal/Service/PropertyServiceImpl.java
+++ hostal/src/main/java/com/example/hostal/Service/PropertyServiceImpl.java
@@ -1,4 +1,35 @@
 package com.example.hostal.Service;
 
-public class PropertyServiceImpl {
+import com.example.hostal.Entity.Property;
+import com.example.hostal.Repository.PropertyRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class PropertyServiceImpl implements PropertyService{
+    @Autowired
+    private PropertyRepository propertyRepository;
+
+    public PropertyServiceImpl(PropertyRepository propertyRepository){
+        super();
+        this.propertyRepository=propertyRepository;
+    }
+    @Override
+    public Property saveProperty(Property property) {
+        return (Property) propertyRepository.save(property);
+    }
+    @Override
+    public Iterable<Property> getAllProperty() {
+        return (List<Property>) propertyRepository.findAll();
+    }
+
+
+    @Override
+    public Property getPropertyById(Integer id) {
+        Optional<Property> propertyOptional = propertyRepository.findById(id);
+        return propertyOptional.orElse(null);
+    }
 }
