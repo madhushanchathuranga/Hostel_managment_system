919567f1cfe3277130ad34100abad54650c6d7b6
A.V.G.M.Chathuranga <79782422+madhushanchathuranga@users.noreply.github.com>
2023-11-12 10:07:37 +0530

    Update ReportServiceImpl.java

--- hostal/src/main/java/com/example/hostal/Service/ReportServiceImpl.java
+++ hostal/src/main/java/com/example/hostal/Service/ReportServiceImpl.java
@@ -1,4 +1,36 @@
 package com.example.hostal.Service;
 
-public class ReportServiceImpl {
+import com.example.hostal.Entity.Report;
+import com.example.hostal.Repository.ReportRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+
+@Service
+public class ReportServiceImpl implements ReportService{
+    @Autowired
+    private ReportRepository reportRepository;
+
+    public ReportServiceImpl(ReportRepository reportRepository){
+        super();
+        this.reportRepository=reportRepository;
+    }
+    @Override
+    public Report saveReport(Report report) {
+        return reportRepository.save(report);
+    }
+    @Override
+    public Iterable<Report> getAllReport() {
+        return (List<Report>) reportRepository.findAll();
+    }
+
+
+    @Override
+    public Report getReportById(Long id) {
+        Optional<Report> reportOptional = reportRepository.findById(id);
+        return reportOptional.orElse(null);
+    }
 }
