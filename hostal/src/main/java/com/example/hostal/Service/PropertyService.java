0323eaddeb3ff4e7804e219bbb79047588e9d503
A.V.G.M.Chathuranga <79782422+madhushanchathuranga@users.noreply.github.com>
2023-11-12 10:00:50 +0530

    Update PropertyService.java

--- hostal/src/main/java/com/example/hostal/Service/PropertyService.java
+++ hostal/src/main/java/com/example/hostal/Service/PropertyService.java
@@ -1,4 +1,9 @@
 package com.example.hostal.Service;
-
+import com.example.hostal.Entity.Property;
 public interface PropertyService {
+    Property saveProperty(Property property);
+
+    Iterable<Property> getAllProperty();
+
+    Property getPropertyById(Integer id);
 }
