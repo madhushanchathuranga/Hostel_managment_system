20ef3f216e10c14b0a9eb664c8ec866b3ec65b1b
A.V.G.M.Chathuranga <79782422+madhushanchathuranga@users.noreply.github.com>
2023-11-12 10:11:14 +0530

    Update StudentServiceImpl.java

--- hostal/src/main/java/com/example/hostal/Service/StudentServiceImpl.java
+++ hostal/src/main/java/com/example/hostal/Service/StudentServiceImpl.java
@@ -1,4 +1,37 @@
 package com.example.hostal.Service;
 
-public class StudentServiceImpl {
+import com.example.hostal.Entity.Property;
+import com.example.hostal.Entity.Student;
+import com.example.hostal.Repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class StudentServiceImpl implements StudentService{
+    @Autowired
+    private StudentRepository studentRepository;
+
+    public StudentServiceImpl(StudentRepository studentRepository){
+        super();
+        this.studentRepository=studentRepository;
+    }
+    @Override
+    public Student saveStudent(Student student) {
+        return (Student) studentRepository.save(student);
+    }
+    @Override
+    public Iterable<Student> getAllStudent() {
+        return (List<Student>) studentRepository.findAll();
+    }
+
+
+    @Override
+    public Student getStudentById(Integer id) {
+        Optional<Student> studentOptional = studentRepository.findById(id);
+        return studentOptional.orElse(null);
+    }
+
 }
