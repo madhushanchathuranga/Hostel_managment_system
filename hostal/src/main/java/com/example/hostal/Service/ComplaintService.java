52b402d7ab1825d2116891d2e0059d7c60c9f352
A.V.G.M.Chathuranga <79782422+madhushanchathuranga@users.noreply.github.com>
2023-11-12 09:50:06 +0530

    Update ComplaintService.java

--- hostal/src/main/java/com/example/hostal/Service/ComplaintService.java
+++ hostal/src/main/java/com/example/hostal/Service/ComplaintService.java
@@ -1,4 +1,9 @@
 package com.example.hostal.Service;
 
 public interface ComplaintService {
+    Complaint saveComplaint(Complaint complaint);
+
+    Iterable<Complaint> getAllComplaint();
+
+    Complaint getComplaintById(Long id);
 }
