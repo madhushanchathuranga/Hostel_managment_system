62d91f7816d838ce14e962460ea002c86855bfbd
A.V.G.M.Chathuranga <79782422+madhushanchathuranga@users.noreply.github.com>
2023-11-13 23:43:31 +0530

    Create RoomRepo.java

---
+++ HMS_BACKEND/src/main/java/com/rhms/hms_backend/Repositories/RoomRepo.java
@@ -0,0 +1,43 @@
+package com.rhms.hms_backend.Repositories;
+
+import com.rhms.hms_backend.Models.Room;
+import com.rhms.hms_backend.Models.Users;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.query.Procedure;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface RoomRepo  extends JpaRepository<Room,Long> {
+
+
+    @Query(nativeQuery = true, value = "SELECT * FROM available_rooms")
+    List<Object[]> findAvailableRoom();
+
+
+    @Query(nativeQuery = true, value = "SELECT * FROM roomassignmentview")
+    List<Object[]> findAssignRoom();
+
+
+    @Query(nativeQuery = true, value = "SELECT * FROM property WHERE property_uniq_id = 'ch4108'")
+    List<Object[]> findProperty();
+
+
+    @Query(nativeQuery = true, value = "SELECT * FROM property WHERE property_uniq_id = :propertyName")
+    List<Object[]> findPropertyByName(@Param("propertyName") String propertyName);
+
+
+
+    @Query(nativeQuery = true, value = "SELECT * FROM property WHERE property_id = :id")
+    List<Object[]> getPropertiesById(@Param("id") String id);
+
+    @Query(value = "SELECT GetRoomCount()", nativeQuery = true)
+    int GetRoomCount();
+
+
+
+
+
+
+}
