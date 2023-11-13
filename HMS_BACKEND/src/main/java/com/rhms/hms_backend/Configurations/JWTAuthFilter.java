bdcca649d190e7ba0c5742c98af97ac7a07327c9
Chamidu Dilshan <84907191+chamidudp@users.noreply.github.com>
2023-11-13 15:42:54 +0530

    JWT

---
+++ HMS_BACKEND/src/main/java/com/rhms/hms_backend/Configurations/JWTAuthFilter.java
@@ -0,0 +1,32 @@
+package com.rhms.hms_backend.Configurations;
+
+import com.rhms.hms_backend.Services.JwtService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.lang.NonNull;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class JWTAuthFilter extends OncePerRequestFilter {
+
+    @Autowired
+    private final JwtService jwtService;
+
+    @Autowired
+    private final UserDetailsService userDetailsService;
+
+    @Override
+   
